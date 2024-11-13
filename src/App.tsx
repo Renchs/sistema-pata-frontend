16d632515d6a6152eff2b3d916bb0ba1d6d5da9d
Renchs <mathfm.dev@gmail.com>
2024-11-13 16:51:10 -0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ModalLogin } from "./components/modalLogin";
+
+export function App() {
+
+  return (
+    <>
+      <div className="flex w-full h-screen justify-center items-center">
+        <ModalLogin />
+      </div>
+    </>
+  )
+}
+
+
