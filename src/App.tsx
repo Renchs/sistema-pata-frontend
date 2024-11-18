e53aab68f2ca49d0fe5c1fb83255cbeb282f2738
Renchs <mathfm.dev@gmail.com>
2024-11-18 13:03:11 -0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 // import { ModalRegistro } from "./components/modalRegistro";
-import { UserList } from "./pages/userList";
+import { Header } from "./components/header";
+// import { UserList } from "./pages/userList";
 
 export function App() {
 
   return (
     <>
-      <div className="flex w-full bg-[#FAFAFA] h-screen justify-center items-center">
-        <UserList />
+      <div className="flex w-full bg-[#FAFAFA] h-screen justify-center items-start">
+        <Header />
       </div>
     </>
   )
