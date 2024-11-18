3980f5a653325bf1cb6477e3fb786e5c33303758
Renchs <mathfm.dev@gmail.com>
2024-11-18 13:26:58 -0300

    feat: react router dom inserido

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 // import { ModalRegistro } from "./components/modalRegistro";
 import { Header } from "./components/header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UsuariosLista } from "./pages/UsuariosLista";
+import { CadastrarPet } from "./pages/CadastrarPet";
 // import { UserList } from "./pages/userList";
 
 export function App() {
 
   return (
     <>
-      <div className="flex w-full bg-[#FAFAFA] h-screen justify-center items-start">
+        <Router>
         <Header />
-      </div>
+        <div className="flex w-full bg-[#FAFAFA] min-h-screen justify-center items-center">
+          <Routes>
+            <Route path="/usuarios" element={<UsuariosLista />} />
+            <Route path="/cadastrar-pet" element={<CadastrarPet />} />
+          </Routes>
+        </div>
+        </Router>
     </>
   )
 }
