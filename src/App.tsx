b2318a119ee7ef5f730b389cd8286ca2fd8a73e2
Renchs <mathfm.dev@gmail.com>
2024-11-19 23:02:32 -0300

    feat: pagina de buscar pet e header do site

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UsuariosLista } from "./pages/UsuariosLista";
 import { CadastrarPet } from "./pages/CadastrarPet";
+import { BuscarPets } from "./pages/BuscarPets";
 // import { UserList } from "./pages/userList";
 
 export function App() {
@@ -11,10 +12,11 @@ export function App() {
     <>
         <Router>
         <Header />
-        <div className="flex w-full bg-[#FAFAFA] min-h-screen justify-center items-center">
+        <div className="flex w-full bg-[#FAFAFA] min-h-[800px] justify-center items-center">
           <Routes>
             <Route path="/usuarios" element={<UsuariosLista />} />
             <Route path="/cadastrar-pet" element={<CadastrarPet />} />
+            <Route path="/pets" element={<BuscarPets />} />
           </Routes>
         </div>
         </Router>
