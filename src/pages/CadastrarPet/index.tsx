245bdee3f7a72dfa29d2690a2e795ed69452033b
Renchs <mathfm.dev@gmail.com>
2024-11-22 02:00:49 -0300

    refactor: form pet

--- src/pages/CadastrarPet/index.tsx
+++ src/pages/CadastrarPet/index.tsx
@@ -1,34 +1,22 @@
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod/src/zod.js";
-import { petFormSchema } from "../../schemas/petValidacao";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { IFormPet, petFormSchema } from "../../schemas/petValidacao";
 import { FormPet } from "../../components/formPet";
 
 
-
-export interface ICadastrarPet {
-    nome: string;
-    especie: string;
-    data_nascimento: string;
-    descricao: string;
-    personalidade: "independente" | "calmo" | "brincalhao";
-    tamanho: "pequeno" | "medio" | "grande";
-}
-
 export function CadastrarPet() {
-    const { control, register, handleSubmit, formState: { errors } } = useForm<ICadastrarPet>({
+    const { control, register, handleSubmit, formState: { errors } } = useForm<IFormPet>({
         resolver: zodResolver(petFormSchema)
     });
 
 
-    const onSubmit = (data: ICadastrarPet) => {
-        console.log("registrou");
+    const onSubmit = (data: IFormPet) => {        
         console.log(data);
     }
 
     return (
         <div className="md:w-[720px] w-full sm:min-h-[600px] flex flex-col gap-4 items-center justify-center shadow-md p-4 bg-white rounded-lg">
-            <FormPet onSubmit={onSubmit} control={control} errors={errors} handleSubmit={handleSubmit} register={register} />
-
+            <FormPet onSubmit={onSubmit} control={control}  errors={errors} handleSubmit={handleSubmit} register={register} />
         </div>
     )
 }
